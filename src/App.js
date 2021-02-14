f084fab90d5ae9eaa39fd5bed986bc688a735d78
Fred <73175901+diverdowngit@users.noreply.github.com>
2021-02-14 17:22:29 +0100

    installed files and images

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
+<<<<<<< Updated upstream
   return (
     <div className="App">
       <h1>Hello</h1>
@@ -9,4 +10,23 @@ function App() {
   );
 }
 
+=======
+    
+    return (
+      <div className="App">
+          <div className='container'>
+              <header>
+                  <div className='header'>
+                      <span className='logo'>Futuristic Cookbook</span>
+                  </div>
+              </header>
+            
+          </div>
+      </div>
+  );
+}
+
+
+
+>>>>>>> Stashed changes
 export default App;
