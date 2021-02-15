3ac8378af1089f1aac4abf4f1b9289ec232e4eca
Massimilianok <massimiliano.rizzuto87@gmail.com>
2021-02-15 16:57:25 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -2,31 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-<<<<<<< Updated upstream
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="container">
+        <header>
+          <div className="header">
+            <span className="logo">Futuristic Cookbook</span>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
 
-=======
-    
-    return (
-      <div className="App">
-          <div className='container'>
-              <header>
-                  <div className='header'>
-                      <span className='logo'>Futuristic Cookbook</span>
-                  </div>
-              </header>
-            
-          </div>
-      </div>
-  );
-}
-
-
-
->>>>>>> Stashed changes
 export default App;
